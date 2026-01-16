e4bd4a1e63c243e821477ab71b0dd3163069212b
Łukasz Strzelewicz <panamapappers97@gmail.com>
2026-01-16 22:56:05 +0100

    init setup

---
+++ playwright.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+  fullyParallel: true,
+  reporter: 'html',
+  use: {
+    // WAŻNE: To jest adres, pod którym GitHub Actions uruchomi Twoją aplikację
+    baseURL: 'http://localhost:5173', 
+    trace: 'on-first-retry',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+});
